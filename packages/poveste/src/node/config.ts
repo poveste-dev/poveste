145c32064e303c5a1d2f7ae5eab466a4887dbe0a
Soryn <soryngitlan@gmail.com>
2026-05-01 20:51:51 +0300

    feat(shared): isolateStyles, globalStyles, StoryLayout.iframeGrid

--- packages/poveste/src/node/config.ts
+++ packages/poveste/src/node/config.ts
@@ -38,6 +38,7 @@ export function getDefaultConfig(): PovesteConfig {
       '**/dist/**',
     ],
     supportMatch: [],
+    isolateStyles: true,
     tree: {
       file: 'title',
       order: 'asc',
