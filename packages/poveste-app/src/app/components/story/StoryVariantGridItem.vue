histoire-dev/histoire

feat(app): sandbox height-sync and iframe-per-grid
SANDBOX_HEIGHT postMessage from sandbox.ts (ResizeObserver + rAF
debounce); StoryVariantSinglePreviewRemote forwards reportedHeight to
the iframe element. StoryVariantGridItem switches grid items to
iframe rendering by default (gated by isolateStyles, override via
StoryLayout.iframeGrid).

packages/histoire-app/src/app/components/story/StoryVariantGridItem.vue
@@ -16,6 +16,7 @@ import { useCurrentVariantRoute } from '../../util/variant'
 import CheckerboardPattern from '../misc/CheckerboardPattern.vue'
 import ToolbarNewTab from '../toolbar/ToolbarNewTab.vue'
 import GenericRenderStory from './GenericRenderStory.vue'
+import StoryVariantSinglePreviewRemote from './StoryVariantSinglePreviewRemote.vue'
 
 const props = defineProps({
   variant: {
@@ -68,6 +69,13 @@ const settings = usePreviewSettingsStore().currentSettings
 
 const contrastColor = computed(() => getContrastColor(settings))
 const autoApplyContrastColor = computed(() => !!histoireConfig.autoApplyContrastColor)
+
+const useIframe = computed(() => {
+  const layout = props.story.layout as any
+  if (layout?.iframeGrid === false) return false
+  if (layout?.iframeGrid === true) return true
+  return histoireConfig.isolateStyles !== false
+})
 </script>
 
 <template>
@@ -137,7 +145,15 @@ const autoApplyContrastColor = computed(() => !!histoireConfig.autoApplyContrast
           'color': autoApplyContrastColor ? contrastColor : undefined,
         }"
       >
+        <StoryVariantSinglePreviewRemote
+          v-if="useIframe"
+          :key="`iframe-${story.id}-${variant.id}`"
+          :story="story"
+          :variant="variant"
+          auto-height
+        />
         <GenericRenderStory
+          v-else
           :key="`${story.id}-${variant.id}`"
           :variant="variant"
           :story="story"

packages/histoire-app/src/app/util/const.ts
@@ -2,3 +2,4 @@ export const STATE_SYNC = '__histoire:state-sync'
 export const SANDBOX_READY = '__histoire:sandbox-ready'
 export const EVENT_SEND = '__histoire:event'
 export const PREVIEW_SETTINGS_SYNC = '__histoire:preview-settings-sync'
+export const SANDBOX_HEIGHT = '__histoire:sandbox-height'

poveste-dev/poveste

packages/poveste-app/src/app/components/story/StoryVariantGridItem.vue
@@ -16,6 +16,7 @@ import { useCurrentVariantRoute } from '../../util/variant'
 import CheckerboardPattern from '../misc/CheckerboardPattern.vue'
 import ToolbarNewTab from '../toolbar/ToolbarNewTab.vue'
 import GenericRenderStory from './GenericRenderStory.vue'
+import StoryVariantSinglePreviewRemote from './StoryVariantSinglePreviewRemote.vue'
 
 const props = defineProps({
   variant: {
@@ -68,6 +69,13 @@ const settings = usePreviewSettingsStore().currentSettings
 
 const contrastColor = computed(() => getContrastColor(settings))
 const autoApplyContrastColor = computed(() => !!povesteConfig.autoApplyContrastColor)
+
+const useIframe = computed(() => {
+  const layout = props.story.layout as any
+  if (layout?.iframeGrid === false) return false
+  if (layout?.iframeGrid === true) return true
+  return povesteConfig.isolateStyles !== false
+})
 </script>
 
 <template>
@@ -139,7 +147,15 @@ const autoApplyContrastColor = computed(() => !!povesteConfig.autoApplyContrastC
           'color': autoApplyContrastColor ? contrastColor : undefined,
         }"
       >
+        <StoryVariantSinglePreviewRemote
+          v-if="useIframe"
+          :key="`iframe-${story.id}-${variant.id}`"
+          :story="story"
+          :variant="variant"
+          auto-height
+        />
         <GenericRenderStory
+          v-else
           :key="`${story.id}-${variant.id}`"
           :variant="variant"
           :story="story"

packages/poveste-app/src/app/util/const.ts
@@ -2,3 +2,4 @@ export const STATE_SYNC = '__poveste:state-sync'
 export const SANDBOX_READY = '__poveste:sandbox-ready'
 export const EVENT_SEND = '__poveste:event'
 export const PREVIEW_SETTINGS_SYNC = '__poveste:preview-settings-sync'
+export const SANDBOX_HEIGHT = '__poveste:sandbox-height'
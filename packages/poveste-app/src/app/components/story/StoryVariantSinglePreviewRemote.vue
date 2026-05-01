85f6d7b4d4b4a43da2771f12796e65e0c1e0a83e
Soryn <soryngitlan@gmail.com>
2026-05-01 20:58:58 +0300

    feat(app): sandbox height-sync and iframe-per-grid
    SANDBOX_HEIGHT postMessage from sandbox.ts (ResizeObserver + rAF
    debounce); StoryVariantSinglePreviewRemote forwards reportedHeight to
    the iframe element. StoryVariantGridItem switches grid items to
    iframe rendering by default (gated by isolateStyles, override via
    StoryLayout.iframeGrid).

--- packages/poveste-app/src/app/components/story/StoryVariantSinglePreviewRemote.vue
+++ packages/poveste-app/src/app/components/story/StoryVariantSinglePreviewRemote.vue
@@ -6,7 +6,7 @@ import { useEventListener } from '@vueuse/core'
 import { computed, onBeforeUnmount, ref, toRaw, watch } from 'vue'
 import { useEventsStore } from '../../stores/events'
 import { usePreviewSettingsStore } from '../../stores/preview-settings'
-import { EVENT_SEND, PREVIEW_SETTINGS_SYNC, SANDBOX_READY, STATE_SYNC } from '../../util/const'
+import { EVENT_SEND, PREVIEW_SETTINGS_SYNC, SANDBOX_HEIGHT, SANDBOX_READY, STATE_SYNC } from '../../util/const'
 import { trackWindow } from '../../util/keyboard'
 import { getSandboxUrl } from '../../util/sandbox'
 import { toRawDeep } from '../../util/state'
@@ -15,6 +15,7 @@ import StoryResponsivePreview from './StoryResponsivePreview.vue'
 const props = defineProps<{
   story: Story
   variant: Variant
+  autoHeight?: boolean
 }>()
 
 const settings = usePreviewSettingsStore().currentSettings
@@ -49,8 +50,10 @@ Object.assign(props.variant, {
   previewReady: false,
 })
 
+const reportedHeight = ref<number | null>(null)
+
 useEventListener(window, 'message', (event) => {
-  switch (event.data.type) {
+  switch (event.data?.type) {
     case STATE_SYNC:
       updateVariantState(event.data.state)
       break
@@ -59,6 +62,11 @@ useEventListener(window, 'message', (event) => {
       break
     case SANDBOX_READY:
       setPreviewReady()
+      break
+    case SANDBOX_HEIGHT:
+      if (typeof event.data.h === 'number') {
+        reportedHeight.value = Math.max(40, event.data.h)
+      }
       break
   }
 })
@@ -153,7 +161,7 @@ function onIframeLoad() {
       :style="isResponsiveEnabled ? {
         width: finalWidth ? `${finalWidth}px` : null,
         height: finalHeight ? `${finalHeight}px` : null,
-      } : undefined"
+      } : (props.autoHeight && reportedHeight ? { height: `${reportedHeight}px` } : undefined)"
       data-test-id="preview-iframe"
       @load="onIframeLoad()"
     />
