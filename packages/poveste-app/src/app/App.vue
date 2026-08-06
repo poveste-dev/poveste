histoire-dev/histoire

feat(app): toggle pane visibility and placement

packages/histoire-app/src/app/App.vue
@@ -19,6 +19,7 @@ import SearchModal from './components/search/SearchModal.vue'
 import GenericMountStory from './components/story/GenericMountStory.vue'
 import StoryList from './components/tree/StoryList.vue'
 import { useCommandStore } from './stores/command'
+import { useLayoutStore } from './stores/layout'
 import { useStoryStore } from './stores/story'
 import { histoireConfig } from './util/config'
 import { onKeyboardShortcut } from './util/keyboard'
@@ -97,6 +98,7 @@ onMounted(() => {
 })
 
 const commandStore = useCommandStore()
+const layoutStore = useLayoutStore()
 </script>
 
 <template>
@@ -140,6 +142,7 @@ const commandStore = useCommandStore()
       />
 
       <BaseSplitPane
+        v-if="layoutStore.settings.storyListVisible"
         save-id="main-horiz"
         :min="5"
         :max="50"
@@ -160,6 +163,10 @@ const commandStore = useCommandStore()
           <RouterView />
         </template>
       </BaseSplitPane>
+      <RouterView
+        v-else
+        class="htw-flex-1 htw-min-h-0"
+      />
     </div>
 
     <LayoutModal

poveste-dev/poveste

packages/poveste-app/src/app/App.vue
@@ -19,6 +19,8 @@ import SearchModal from './components/search/SearchModal.vue'
 import GenericMountStory from './components/story/GenericMountStory.vue'
 import StoryList from './components/tree/StoryList.vue'
 import { useCommandStore } from './stores/command'
+import { useLayoutStore } from './stores/layout'
+import { useLayoutStore } from './stores/layout'
 import { useStoryStore } from './stores/story'
 import { povesteConfig } from './util/config'
 import { onKeyboardShortcut } from './util/keyboard'
@@ -97,6 +99,7 @@ onMounted(() => {
 })
 
 const commandStore = useCommandStore()
+const layoutStore = useLayoutStore()
 </script>
 
 <template>
@@ -141,6 +144,7 @@ const commandStore = useCommandStore()
         />
 
         <BaseSplitPane
+          v-if="layoutStore.settings.storyListVisible"
           save-id="main-horiz"
           :min="5"
           :max="50"
@@ -161,6 +165,10 @@ const commandStore = useCommandStore()
             <RouterView />
           </template>
         </BaseSplitPane>
+        <RouterView
+          v-else
+          class="ptw-flex-1 ptw-min-h-0"
+        />
       </div>
 
       <LayoutModal