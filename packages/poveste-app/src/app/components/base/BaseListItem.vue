histoire-dev/histoire

fix(app): histoire-base-list-ite typo

packages/histoire-app/src/app/components/base/BaseListItem.vue
@@ -14,7 +14,7 @@ function handleNavigate() {
 
 <template>
   <a
-    class="istoire-base-list-ite htw-flex htw-items-center htw-gap-2 htw-text-gray-900 dark:htw-text-gray-100"
+    class="histoire-base-list-ite htw-flex htw-items-center htw-gap-2 htw-text-gray-900 dark:htw-text-gray-100"
     :class="[
       $attrs.class,
       isActive

poveste-dev/poveste

packages/poveste-app/src/app/components/base/BaseListItem.vue
@@ -14,7 +14,7 @@ function handleNavigate() {
 
 <template>
   <a
-    class="istoire-base-list-ite ptw-flex ptw-items-center ptw-gap-2 ptw-text-gray-900 dark:ptw-text-gray-100"
+    class="poveste-base-list-item ptw-flex ptw-items-center ptw-gap-2 ptw-text-gray-900 dark:ptw-text-gray-100"
     :class="[
       $attrs.class,
       isActive